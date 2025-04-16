3fb274a54979d75eb82d1164472726ea383c4a0a
Catherine Koo <catherinek@neoseeker.com>
2025-04-16 09:43:11 -0700

    fixed fixed E_DEPRECATED errors in php 8, from offsetExists, offsetGet, offsetSet, offsetUnset

--- src/com/amazon/paapi5/v1/BrowseNodeChildren.php
+++ src/com/amazon/paapi5/v1/BrowseNodeChildren.php
@@ -198,7 +198,7 @@ class BrowseNodeChildren implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists(mixed $offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -210,7 +210,7 @@ class BrowseNodeChildren implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -223,7 +223,7 @@ class BrowseNodeChildren implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet(mixed $offset, mixed $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -239,7 +239,7 @@ class BrowseNodeChildren implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void
     {
         unset($this->container[$offset]);
     }
